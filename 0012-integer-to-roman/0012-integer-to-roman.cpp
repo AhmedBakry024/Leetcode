56107bb60e1b4114d72e3594a04b5dd84c026af9
Ahmed Bakry <ahmedbakry024@gmail.com>
2023-12-23 19:16:49 +0200

    Time: 4 ms (75.54%), Space: 6.3 MB (95.07%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,29 +2,18 @@ class Solution {
 public:
     string intToRoman(int num) {
         string s = "";
-        map<int,string> m;
-            m[1000]= "M";
-            m[900] = "CM";
-            m[500] = "D";
-            m[400] = "CD";
-            m[100] = "C";
-            m[90] = "XC";
-            m[50] = "L";
-            m[40] =  "XL";
-            m[10] = "X";
-            m[9] = "IX";
-            m[5] = "V";
-            m[4] = "IV";
-            m[1] = "I";
+        int numbers[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-            map<int, string>::reverse_iterator it;
-
-            for(it = m.rbegin(); it != m.rend(); it++){
-                while(num >= it -> first){
-                    s += it -> second;
-                    num -= it->first;
+        
+            for(int i = 0; i < 13; i++){
+                while(num >= numbers[i]){
+                    s += roman[i];
+                    num -= numbers[i];
+                    
                 }
             }
+        
         return s;
     }
 };
