cfc3db3bfa7042d82d9a103cdcee2d6d415716ef
Ahmed Bakry <ahmedbakry024@gmail.com>
2024-07-05 11:23:58 +0300

    Time: 0 ms (100%), Space: 10.7 MB (91.67%) - LeetHub

---
+++ 0099-longest-common-prefix/0099-longest-common-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 1){
+            return strs[0];
+        }
+        int minNum = 201;
+        for(int i = 0;i< strs.size(); i++){
+            if (strs[i].length() < minNum) {minNum = strs[i].length();}
+        }
+        string result = "";
+        for (int i = 0; i < minNum; i++){
+            char cur = strs[0][i];
+            for(int j = 1; j < strs.size(); j++){
+                if(strs[j][i] != cur){
+                    return result;
+                }
+                // if(j == strs.size() -1){
+                    
+                // }
+            }
+            result += cur;
+        }
+        return result;
+    }
+};
