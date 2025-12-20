0c1652db651c479c104a0e0ed25669ca2984f18b
احمد بكري <ahmedbakry024@gmail.com>
2025-12-20 19:28:57 +0200

    0118-PASCALS-TRIANGLE Stats: Time: 0 ms (100%), Space: 9.8 MB (31.92%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
-        if (n == 0)
-            return {};
-        if (n == 1)
-            return {{1}};
-        vector<int> prev;
-        vector<vector<int>> ans;
-        for (int i = 1; i <= n; i++) {
-            vector<int> cur(i);
-            for (int j = 0; j < i; j++) {
-                if (j == 0 || j == i - 1) {
-                    cur[j] = 1;
-                    continue;
-                }
-                cur[j] = prev[j - 1] + prev[j];
+        vector<vector<int>> ans = {{1}};
+        if(n == 1) return ans;
+        ans.push_back({1,1});
+        vector<int> prev = ans[1];
+        for(int i = 2; i< n;i++){
+            vector<int> cur(i+1,1);
+            for(int j=1;j<cur.size()-1;j++){
+                cur[j] = prev[j] + prev[j-1];
             }
-            ans.push_back(cur);
             prev = cur;
+            ans.push_back(cur);
         }
+        
         return ans;
     }
 };
